c94c59bda001156a06b43f49d957115a5282ce00
Andrea <you@example.com>
2017-08-15 15:09:34 -0400

    working

--- parallax-template/js/PetMatch.js
+++ parallax-template/js/PetMatch.js
@@ -1,95 +1,87 @@
 
-// function doCalculation(element, dfc) {
-//     var form = document.getElementById('dogfoodcalc');
-//     var answer;
-//     var answer1;
-//     var answer2;
-//     var feedYourDog;
-//     var bagLast;
-//     var weight = form.weight.value;
-//     var unit = "cup";
-//     for (i = 0; i < form.unit.length; i++)
-//     {
-//             unit = form.unit[i].value;
-//     }
-//     var measure = "lb";
-//     for (i = 0; i < form.measure.length; i++)
-//     {
-//             measure = form.measure[i].value;
-//     }
-//     var activity_level = form.activity_level.value.split(",");
-//     var calories = form.calories.value;
-//     if (element.value == "") {
-//         if (!dfc)
-//          document.getElementById('response').innerHTML = '';
-//         return;
-//     }
-//     if (unit == "cup")
-//         var type = " cups";
-//     else 
-//         type = " kg";
-//     if (measure == 'lb')
-//         weight/=2.2;;
-//     if ((weight == "" || activity_level[0] == "" || measure == ""))
-//         return;
-//         answer = (Math.pow(weight, 0.75) * activity_level[0]);
-//         yourDogRequires = "<p>Your dog requires <strong>" + Math.round(answer) + "</strong> kcal / day. ";       
-//         feedYourDog = "Feed your dog <strong>" + (answer / calories).toFixed(2) + type + "</strong> / day. ";
-//         bagLast = "A 15 pound bag of dog food would last <strong>" + Math.round(15 / ((answer / calories)*0.2875)) +"</strong> days.</p>";
-        
-//         //Return the results
-//         document.getElementById('response').innerHTML = "<div class=\"requirements\">" + yourDogRequires + feedYourDog + bagLast +"</div>";
- 
-// }
-
-// function resetCalc(resetButton) {
-//     var form = document.getElementById('dogfoodcalc');
-//     document.getElementById('response').innerHTML = '';
-//     form.calories.value = '';
-//     form.activity_level.selectedIndex = 0;
-//     if (resetButton)
-//         form.reset();
-// }
-
-
-//    $(function() {
-//   // Initialize form validation 
-//   $("form[name='dogfoodcalc']").validate({
-//      // Specify validation rules
-//     rules: {
-//       // The key name on the left side is the name attribute
-//       // of an input field. Validation rules are defined
-//        // on the right side
-//      weight: "required",
-//      calories: "required",
-//      activity_level:"required"
-      
-//     },
-//     // Specify validation error messages
-//    messages: {
-//      weight: "Please enter your dog's weight",
-//      calories: "Please enter your dog's food calories per serving",
-//     activity_level: "Please select your dog's activity level"
-//    },
-//      });
-// });
-
-
 
 
  $(document).ready(function(){
+
     event.preventDefault();
 
-      $("#table").hide()
+    $("#table").hide()
 
-      $("#cost").on("click", function(){
+    $("#cost").on("click", function(){
           $("#table").toggle()
+    });
+
+    var quiz = {
+      question: "What size dog do you want?",
+      answers: ["Small", "Medium", "Large"],
+    };
+
+     var small = [ "Chihuahua", "Yorkie","Pug", "Beagle"];
+ var medium = ["Pit-Bull",  "Boxer","Poodle","Golden-Retriever"];
+ var large = ["German-Shepherd", "Great Dane", "Husky", "Dalmation"];
+
+    
+    $("#quiz").append("<h2>" + quiz.question + "</h2><br>" + "<button id='small' style='font-size:18px'><i class='fa fa-paw'></i> " + quiz.answers[0] + "</button>"+ "<button id='medium' style='font-size:18px'><i class='fa fa-paw'></i>" + quiz.answers[1] +"</button>" + "<button id='large' style='font-size:18px'><i class='fa fa-paw'></i>" + quiz.answers[2] + "</button>");
+  
+
+    
+
+    $("#small").on("click", function(){
+      $("#options").empty();
+
+      console.log("work");
+
+      for (var i =0; i < small.length; i++){
+
+      var pickSmall = $("<button id ='work'>");
+
+      pickSmall.text(small[i]);
+
+      $("#options").append(pickSmall);
+    }
+
+   
+    });
+
+     $("#medium").on("click", function(){
+       $("#options").empty();
+
+      for (var i =0; i < medium.length; i++){
+
+      var pickMedium = $("<button id ='work'>");
+
+      pickMedium.text(medium[i]);
+
+      $("#options").append(pickMedium);
+    }
       });
+
+      $("#large").on("click", function(){
+         $("#options").empty();
+      for (var i =0; i < large.length; i++){
+
+      var pickLarge = $("<button id ='work'>");
+
+      pickLarge.text(large[i]);
+
+      $("#options").append(pickLarge);
+    }
+    });
+
+      $(document).on("click", '#work', function(){
+        console.log("please");
+
+        var pickDog = $(this).text();
+        $("#search").val(pickDog);
+      });
+
+    
+
 
       var breed = "";
       
 
-    $("button").click(function(){
+    $(".find").click(function(){
 
       var breed = $("#search").val().trim();
       var name = $("#vistor").val().trim();
@@ -146,8 +138,9 @@
    
     success:(function(data){
     
- 
 
+ 
+      $("#pet-view").prepend("<div>" + "<h2>" + "Here is your " + breed + "</h2></div>")
     
       $("#photo").html("<img src="+data.petfinder.pets.pet.media.photos.photo[3].$t+"></img>");
     
@@ -173,7 +166,7 @@
   $("#location").val("");
   
 
-
+  function getName(){
       // Initialize Firebase
        var config = {
           apiKey: "AIzaSyDuBFYzfIw_9JwSE3U0_dkWvbUt2OhV72Q",
@@ -183,7 +176,7 @@
           storageBucket: "petmatch-b3da8.appspot.com",
           messagingSenderId: "1014252033957"
         };
-        
+
   
         firebase.initializeApp(config);
 
@@ -194,21 +187,32 @@
           Breed: breed,
           Zip_Code: zipCode
         }
-        database.ref().push(user);
 
-  var baseURL = "https://api.petfinder.com/";
-  var reqType = "pet.find?";
-  var params = "breed="+ breed + "&location=" + location + "&count=1&output=basic&";
-  var yourKey = "key=ad2cd8f7ea4a77a23e2a8bb6b66b4b52&";
-  var format = "format=json";
-  var callback = "&callback=?";
+        database.ref().push(user);
+    
+
+
+   database.ref().on("child_added", function(childSnapshot) {
+
+    var vistorName = childSnapshot.val().name
+    console.log(vistorName)
+  
+
+    $("#marque").append("<h1>" + "Thank you" + vistorName + "for visting our page!" + "<i class='fa fa-paw' style='font-size:36px'></i>" + "</h1>" )
+ 
+   });
+
+ };
+
+
+
   
 
     
   
 
     });
-
+ getName ();
       
   });
 
